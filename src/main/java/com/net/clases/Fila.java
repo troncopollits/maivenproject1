7e73efa74bb7e2cbfeaea9402cae6597f17a0155
artur <artur@DESKTOP-9J1LRN4.home>
2018-10-03 12:46:48 +0200

    Actualización de la clase Fila.

---
+++ src/main/java/com/net/clases/Fila.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.net.clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author artur
+ */
+public class Fila {
+     Integer i;
+    ArrayList lista;
+
+    public Integer getI() {
+        return i;
+    }
+
+    public void setI(Integer i) {
+        this.i = i;
+    }
+
+    public ArrayList getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList lista) {
+        this.lista = lista;
+    }
+    
+}
