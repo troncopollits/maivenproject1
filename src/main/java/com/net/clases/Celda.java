aa748697f005e5834e6eed833c6c4e2a4404f102
artur <artur@DESKTOP-9J1LRN4.home>
2018-10-03 12:46:34 +0200

    Actualización de la clase Celda.

---
+++ src/main/java/com/net/clases/Celda.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.net.clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author artur
+ */
+public class Celda {
+   Integer i,j,res;
+
+    public Integer getI() {
+        return i;
+    }
+
+    public void setI(Integer i) {
+        this.i = i;
+    }
+
+    public Integer getJ() {
+        return j;
+    }
+
+    public void setJ(Integer j) {
+        this.j = j;
+    }
+
+    public Integer getRes() {
+        return res;
+    }
+
+    public void setRes(Integer res) {
+        this.res = res;
+    }
+   
+    
+}
